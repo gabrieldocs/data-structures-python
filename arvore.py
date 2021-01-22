da40bcb26e1c939b642410a557a066dca8de0ecf
Lucas Gabriel Guilherme dos Santos <mydocs.lucas@gmail.com>
2021-01-22 14:45:09 -0300

    Inserindo método de busca de valor na árvore
    O método find percorre a árvore binária realizando comparações em profundidade (desde que os elementos da árvore sejam inseridos de forma ordenada e crescentes) e retorna uma String;
    Elementos são inseridos respeitando a condição de: menores valores à esquerda e maiores valores à direita;

--- arvore.py
+++ arvore.py
@@ -1,9 +1,11 @@
 class Node:
+    #inicializa a árvore
     def __init__(self, data):
         self.left = None
         self.right = None
         self.data = data 
-    
+        
+    #imprime os valores armazenados na árvore binária de forma recursiva
     def PrintValue(self):
         if self.left:
             self.left.PrintValue()
@@ -11,7 +13,7 @@ class Node:
             self.right.PrintValue()
         print(self.data)
         
-
+    #insere um valor à esquerda ou à direita do nó raíz
     def insert(self, data):
         if self.data:
             if data < self.data:
@@ -26,10 +28,23 @@ class Node:
                     self.right.insert(data)
         else:            
             self.data = data
+    
+    #encontra um valor na árvore e retorna uma string
+    def find(self, value):
+        if value < self.data:
+            if self.left is None:
+                return str(value)+" Não foi encontrado"
+            return self.left.findval(value)
+        elif value > self.data:
+            if self.right is None:
+                return str(value)+"Não foi encontrado"
+            return self.right.findval(value)
+        else:
+            print(str(self.data) + ' Foi encontrado')
 
 
 
-
+#instancia a árvore
 root = Node(10)
 root.insert(5)
 root.insert(6)
@@ -37,3 +52,7 @@ root.insert(81)
 root.insert(17)
 
 root.PrintValue()
+
+#verifica existência dos valores
+print(root.find(17))
+print(root.find(14))
